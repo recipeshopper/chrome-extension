870c90c986365af34662c4336fd5a4efc79ee6c3
nchen0 <nicky.chen0@gmail.com>
2018-11-03 19:48:38 -0700

    chrome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  getIngredients() {
+    console.log(document.getElementsByClassName("recipe-ingred_txt"));
+    let ingrArr = [];
+    let temp = document.getElementsByClassName("recipe-ingred_txt");
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i].getAttribute("itemprop") === "recipeIngredient") {
+        ingrArr.push(temp[i].textContent);
+      }
+    }
+    console.log("ingrArr: ", ingrArr);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <div>
+            <button onClick={this.getIngredients}>Click me</button>
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
